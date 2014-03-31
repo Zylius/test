fb3f9fff18ac1c69ce2e5cf0ca250dd1f1bca5a2
Zylius <Zylius1@gmail.com>
2014-03-31 15:40:44 +0300

    Testing

--- Tests/arrayTest.php
+++ Tests/arrayTest.php
@@ -4,7 +4,7 @@ namespace Tests{
 
 require_once 'PHPUnit/Autoload.php';
 
-class StackTest extends PHPUnit_Framework_TestCase
+class StackTest extends \PHPUnit_Framework_TestCase
 {
     public function testPushAndPop()
     {
